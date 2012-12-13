d70953d6fbf4736f40679defbb9d38c0f682cae3
Pierre-Yves Gerardy <pygy79@gmail.com>
2012-12-13 23:21:44 +0100

    Cleaned the example.

--- examples/foo.js
+++ examples/foo.js
@@ -2,24 +2,25 @@ var STOMPed = require ('../index');
 
 var log = console.log.bind(console);
 
-var S = new STOMPed("stomp://ec2-54-234-15-174.compute-1.amazonaws.com:6782", {STOMPedDebug:true}, function(headers){
+// replace the address with the one of your server.
+var S = new STOMPed("stomp://ec2-54-234-15-174.compute-1.amazonaws.com:6782", {}, function(headers){
     console.log('Connected!');
     var sub = new S.Subscription('/queue/foo',log);
     sub.on('MESSAGE',log)
-    S.send('/queue/foo', {}, 'GERTERTERTER')
-    S.send('/queue/foo', {}, 'GERTERTERTER')
-    process.nextTick(function(){
-        tr = new S.Transaction(function(frame){
-            log('Transaction',this,frame)
-            this.send('/queue/foo', {}, "brabar")
-            this.send('/queue/foo', {}, "bribirOOOO")
-            this.commit()
-        })    
-    })
+    S.send('/queue/foo', {}, 'Elephant')
+    S.send('/queue/foo', {}, 'Lemon')
+
+    tr = new S.Transaction(function(frame){
+        log('Transaction',this,frame)
+        this.send('/queue/foo', {}, "Message...")
+        this.send('/queue/foo', {}, "... in a bottle.")
+        this.commit()
+    })    
 
 });
+
 S.on('MESSAGE', function(m){
-    console.log('Message: ', m)
+    console.log('Message: '.yellow, m.body)
 })
 
 S.enableDebug();
